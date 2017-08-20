7d6c47dd475ed58f4b22e480a717a3caa07fa421
William Kimball <github-rspec-puppet-yaml@kimballstuff.com>
2017-08-20 10:02:33 -0500

    Created from bundler's gem wizard

---
+++ lib/rspec/puppet/yaml.rb
@@ -0,0 +1,9 @@
+require "rspec/puppet/yaml/version"
+
+module Rspec
+  module Puppet
+    module Yaml
+      # Your code goes here...
+    end
+  end
+end
---
+++ lib/rspec/puppet/yaml/version.rb
@@ -0,0 +1,7 @@
+module Rspec
+  module Puppet
+    module Yaml
+      VERSION = "0.1.0"
+    end
+  end
+end
