e7e8286a37f531d216d24d3dd93ab9b047912126
William Kimball <30981667+wwkimball@users.noreply.github.co>
2017-09-04 16:18:28 -0500

    Added missing YARD doc info

--- lib/rspec-puppet-yaml/extenders.rb
+++ lib/rspec-puppet-yaml/extenders.rb
@@ -1,6 +1,13 @@
 # Identifies a YAML data file based on the name of a *_spec.rb rspec file and
 # passes it to `parse_rspec_puppet_yaml` to initiate parsing.
 #
+# @param rspec_file Path to and name of the RSpec *_spec.rb file.  It is easiest
+#  to simply pass `__FILE__` to this function from that file.
+#
+# @example Typical use
+#  require 'spec_helper'
+#  parse_yaml_from_spec(__FILE__)
+#
 # @since 0.1.0
 def parse_yaml_from_spec(rspec_file)
   if rspec_file =~ /^(.+)_spec\.rb$/
